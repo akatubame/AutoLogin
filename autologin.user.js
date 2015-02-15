26f121abc299cc7917c5497129a69187e4987bd6
akatubame <kurotubame5@gmail.com>
2015-02-15 21:48:14 +0900

    Update autologin.user.js

--- autologin.user.js
+++ autologin.user.js
@@ -21,9 +21,9 @@
 //  ・inputs：入力するフォームのINPUTタグ (複数指定可)
 //     ┃
 //     ┣ input1 ━┳ xpath (INPUTタグのXPath)
-//     ┃　　　　　┗ text  (入力する内容)
+//     ┃         ┗ text  (入力する内容)
 //     ┣ input2 ━┳ xpath
-//     ┃ 　　　 　┗ text 
+//     ┃         ┗ text 
 //     ┗ submit ━━ xpath (入力後にクリックするINPUTタグのXPath)
 // ---------------------------------------------------------------------------
 var site = {
